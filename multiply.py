fbfb6b8689a36540049a153dc5348674e9ac83e0
Aakash <8xaakash@gmail.com>
2023-07-17 09:53:41 +0530

    Add checks for proper input
    Multiply.py now checks if the user input is an int and outputs 'Please enter a number' if not an int

--- multiply.py
+++ multiply.py
@@ -1,4 +1,10 @@
-a = int(input("Enter a number: "))
-b = int(input("Enter a number: "))
+a = input("Enter a number: ")
+b = input("Enter a number: ")
 
-print(a * b)
+try:
+    a = int(a)
+    b = int(b)
+except ValueError: 
+    print("Please enter a number")
+else:
+    print(a * b)
